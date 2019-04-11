7ace43679a6563f7a9775b6597244a43b99a8142
Karen Garcia <karen.garcia@nexscreen.com>
2019-04-10 20:09:44 -0600

    added js for name

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
+/* 
+input items:
+	Range: minRange / maxRange
+	Names: namePlayerOne / namePlayerTwo
+	Guess: guessPlayerOne / guessPlayerTwo
 
+change points:
+	Range: startNum / endNum
+	Name: resultNamePlayerOne / resultNamePlayerTwo
+	Result: playerOneRecordGuess / playerTwoRecordGuess
+  Comments: funnyResultOne / funnyResultTwo
+  
+Functions:
+  minFunction() - Start number
+  maxFunction() - End number
+  nameOneFunction() - Player One Name
+  nameTwoFunction() - Player Two Name
+
+*/
 function minFunction () {
   document.getElementById("startNum").innerHTML = document.getElementById("minRange").value;
 }
 
-
-
 function maxFunction () {
   document.getElementById("endNum").innerHTML = document.getElementById("maxRange").value;
 }
+
+function nameOneFunction (){
+  document.getElementById("resultNamePlayerOne").innerHTML = document.getElementById("namePlayerOne").value;
+}
+
+function nameTwoFunction (){
+  document.getElementById("resultNamePlayerTwo").innerHTML = document.getElementById("namePlayerTwo").value;
+}
+
