515ea6cff6b63a9c6334015dabd25e092e63afc2
Karen Garcia <karen.garcia@nexscreen.com>
2019-04-10 19:26:26 -0600

    added Challenger 2 input and labels. Added buttons submit/reset/clear

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+
 function minFunction () {
   document.getElementById("startNum").innerHTML = document.getElementById("minRange").value;
 }
+
+
 
 function maxFunction () {
   document.getElementById("endNum").innerHTML = document.getElementById("maxRange").value;
