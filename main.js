367575ba23175ed4e9be0f12af6fb4f508cb7639
Karen Garcia <karen.garcia@nexscreen.com>
2019-04-10 18:58:51 -0600

    added js to pull min and max output into current range

---
+++ main.js
@@ -0,0 +1,7 @@
+function minFunction () {
+  document.getElementById("startNum").innerHTML = document.getElementById("minRange").value;
+}
+
+function maxFunction () {
+  document.getElementById("endNum").innerHTML = document.getElementById("maxRange").value;
+}
