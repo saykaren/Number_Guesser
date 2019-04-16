9dbeffbc2e34b46af2df57c5a66e002d1302609a
Karen Garcia <karen.garcia@nexscreen.com>
2019-04-16 10:41:27 -0600

    got js to do random number and record players results

--- main.js
+++ main.js
@@ -25,38 +25,46 @@ function minMaxFunction () {
   document.getElementById("endNum").innerHTML = document.getElementById("maxRange").value;
   let endNum = document.getElementById("endNum").innerHTML;
   console.log("we are ending at "+endNum);
-
-  function myRandomInt (startNum, endNum) {
-    let ranAnswer = parseInt(Math.random() * (endNum-startNum) + startNum);
-    console.log("Random Number is " + ranAnswer);
-    let playerOneResult = document.getElementById('playerOneRecordGuess').innerHTML;
-    console.log("Player one result is "+playerOneResult);
-  }
+  return startNum, endNum;
 
 
 }
+
+
+
+
+/*example of random function that works
+function myRandomInt (startNum, endNum) {
+  let ranAnswer = parseInt(Math.random() * (endNum-startNum) + startNum);
+  console.log("Random Number is " + ranAnswer);
+  let playerOneResult = document.getElementById('playerOneRecordGuess').innerHTML;
+  console.log("Player one result is "+playerOneResult);
+} */
+
 
 function myRandomInt (startNum, endNum) {
   let ranAnswer = parseInt(Math.random() * (endNum-startNum) + startNum);
   console.log("Random Number is " + ranAnswer);
   let playerOneResult = document.getElementById('playerOneRecordGuess').innerHTML;
   console.log("Player one result is "+playerOneResult);
+  let playerTwoResult = document.getElementById('playerTwoRecordGuess').innerHTML;
+  console.log("Player two result is "+playerTwoResult);
 }
-
-function karenRandomInt (min, max) {
-  let ranAnswer = parseInt(Math.random() * (max-min) + min);
-  console.log("Random number is " + ranAnswer);
-  let playerOneResult = document.getElementById('playerOneRecordGuess').innerHTML;
-  console.log("Player one result is "+playerOneResult);
-  
-}
-
 
 function nameFunctionAndGuess () {
   document.getElementById("resultNamePlayerOne").innerHTML = document.getElementById("namePlayerOne").value;
   document.getElementById("resultNamePlayerTwo").innerHTML = document.getElementById("namePlayerTwo").value;
   document.getElementById("playerOneRecordGuess").innerHTML = document.getElementById("guessPlayerOne").value;
   document.getElementById("playerTwoRecordGuess").innerHTML = document.getElementById("guessPlayerTwo").value;
+
+  document.getElementById("startNum").innerHTML = document.getElementById("minRange").value;
+  let startNum = document.getElementById("startNum").innerHTML;
+  console.log("we are starting at "+startNum);
+  
+  document.getElementById("endNum").innerHTML = document.getElementById("maxRange").value;
+  let endNum = document.getElementById("endNum").innerHTML;
+  console.log("we are ending at "+endNum);
+  myRandomInt(startNum, endNum);
 }
 
 function myResetButton () {
